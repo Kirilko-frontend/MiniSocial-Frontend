5fea974878fdcab7ca9a6eb1230c2ac7868d1d1e
Kirilko-frontend <kkir614@gmail.com>
2025-09-02 12:55:04 +0200

    Deploy link add

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://minisocial-backend-91oo.onrender.com",
   withCredentials: true,
 });
 
